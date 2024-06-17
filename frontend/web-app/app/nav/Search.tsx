65c907252c4f73104762ab30ed81976e9e1a41c1
N1kakk <veronikakolesova2003@gmail.com>
2024-06-17 22:24:37 +0300

    Delete auction

--- frontend/web-app/app/nav/Search.tsx
+++ frontend/web-app/app/nav/Search.tsx
@@ -1,11 +1,13 @@
 ﻿'use client'
 
 import { useParamsStore } from '@/hooks/useParamsStore';
+import { usePathname, useRouter } from 'next/navigation';
 import React, { useState } from 'react'
 import { FaSearch } from 'react-icons/fa'
 
 export default function Search() {
-
+    const router = useRouter();
+    const pathName = usePathname();
     const setParams = useParamsStore(state => state.setParams);
     const setSearchValue = useParamsStore(state => state.setSearchValue);
     const searchValue = useParamsStore(state => state.searchValue);
@@ -15,6 +17,8 @@ export default function Search() {
     }
 
     function search(){
+        if(pathName !== '/') router.push('/');
+        
         setParams({searchTerm: searchValue});
     }
 
